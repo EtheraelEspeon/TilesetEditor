01b4f1066492a3feb6ce4fcaa933247b5ac430af
Emily <louis_d@live.com>
2024-04-09 23:01:26 -0400

    Make buttons darken when selected

--- src/PaletteEditor.cpp
+++ src/PaletteEditor.cpp
@@ -10,16 +10,20 @@ void PaletteEditor::Update(Rectangle size){
 
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BASE_COLOR_NORMAL, 0x00000000); // make button bodies transparent
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BORDER_COLOR_NORMAL, 0x00000040); // make borders partially transparent when unselected
-	
+
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BASE_COLOR_FOCUSED, 0x00000040); // make the button body darken when focused
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BORDER_COLOR_FOCUSED, 0x00000060); // make the button border darken when focused
 
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BASE_COLOR_PRESSED, 0x00000080); // make buttons get darker when pressed
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BORDER_COLOR_PRESSED, 0x000000AA); // make the border darker when pressed
 
+	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BASE_COLOR_DISABLED, 0x00000000); // make the button body transparent when the color is selected
+	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::BORDER_COLOR_DISABLED, 0x000000AA); // darken the border when the color is selected
+
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::TEXT_COLOR_NORMAL, 0x000000FF);
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::TEXT_COLOR_FOCUSED, 0x000000FF);
 	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::TEXT_COLOR_PRESSED, 0x000000FF);
+	GuiSetStyle(GuiControl::BUTTON, GuiControlProperty::TEXT_COLOR_DISABLED, 0x000000FF);
 
 	GuiSetStyle(GuiControl::DEFAULT, GuiDefaultProperty::TEXT_SIZE, 30);
 
@@ -39,7 +43,11 @@ void PaletteEditor::Update(Rectangle size){
 			DrawRectangleRec(bounds, TilesetData::GetColor(buttonIdx));
 
 			std::string idxStr = std::to_string(buttonIdx);
+			
+			// make the selected color's button disabled to highlight its border
+			if(buttonIdx == TilesetData::GetActiveColorIdx()) GuiDisable();
 			if(GuiButton(bounds, idxStr.c_str())) TilesetData::SetActiveColor(buttonIdx);
+			if(buttonIdx == TilesetData::GetActiveColorIdx()) GuiEnable();
 			
 			buttonIdx++;
 		}
