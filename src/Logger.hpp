635656175d6202f86881bea467a08789bcec68e5
Emily <louis_d@live.com>
2024-04-09 23:08:20 -0400

    Add documentation

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -6,9 +6,13 @@
 class Logger {
 	public:
 	enum class Severity : uint8_t {
+		// for messages while debugging, there's no reason to print these in release mode
 		Debug = 0,
+		// for messages saying tasks have completed, like file i/o
 		Message = 1,
+		// for situations where the state of the program can't be changed as intended, but isn't invalidated. i.e., failing to set color idx 0
 		Warning = 2,
+		// for when you have to break an invariant, like setting a non-nullable pointer to null
 		Error = 3
 	};
 
